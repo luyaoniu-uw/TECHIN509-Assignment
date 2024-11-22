8b98155da49eadba0a390058d6d447f3fb51b747
Laura <lauratan2000@outlook.com>
2024-11-22 15:55:52 -0800

    add verification of colors

--- turtle_race.py
+++ turtle_race.py
@@ -13,6 +13,8 @@ bet = s.textinput(title="Make your Bet", prompt="Which turtle which win ? Enter 
 X = -230
 Y = -100
 colors = ["red", "orange", "yellow", "blue", "violet"]
+while bet not in colors:
+    bet = s.textinput(title="Make your Bet", prompt="Please choose a valid color (red, orange, yellow, blue, violet): ")
 
 turtles = []
 for i in range(0, 5):
