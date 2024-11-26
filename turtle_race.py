fd036398f56676e0abe34fd85a9917b93ed7d8cc
Hannah <hx2313@uw.edu>
2024-11-26 15:54:50 -0800

    Add record_game_data function and update turtle race file

--- turtle_race.py
+++ turtle_race.py
@@ -1,14 +1,12 @@
-# code credit: https://www.geeksforgeeks.org/turtle-race-game-using-python-turtle-graphics-library/
-
-
 import random
 from turtle import Turtle, Screen
+from record_game import record_game_data
 
 Race = False
 
 s = Screen()
 s.setup(width=500, height=400)
-bet = s.textinput(title="Make your Bet", prompt="Which turtle which win ? Enter Color : ")
+bet = s.textinput(title="Make your Bet", prompt="Which turtle will win? Enter Color:")
 X = -230
 Y = -100
 colors = ["red", "orange", "yellow", "blue", "violet"]
@@ -18,7 +16,7 @@ for i in range(0, 5):
     t = Turtle(shape="turtle")
     t.color(colors[i])
     t.penup()
-    t.goto(x=X, y=-100 + 50 * i)
+    t.goto(x=X, y=50 * i)
     turtles.append(t)
 
 if bet:
@@ -27,16 +25,18 @@ if bet:
 while Race:
 
     for turtle in turtles:
+        
+        distance = random.randint(0, 10)
+        turtle.forward(distance)
 
         if turtle.xcor() >= 230:
             Race = False
             winning = turtle.pencolor()
             if winning == bet:
-                print(f"You have Won the bet on {winning} turtle! the {winning} is the winner")
+                print(f"You have Won the bet on {winning} turtle! The {winning} is the winner")
             else:
-                print(f"You lose! {winning} turtle is winner")
+                print(f"You lose! {winning} turtle is the winner")
+            record_game_data(bet, winning)
 
-        distance = random.randint(0, 10)
-        turtle.forward(distance)
 
 s.exitonclick()
