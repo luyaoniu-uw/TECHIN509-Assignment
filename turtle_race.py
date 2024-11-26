a80f4869e077c938f6d318ace0f3b2002ee32e96
Lydia <1@a.com>
2024-11-25 18:42:01 -0800

    Revise turtle_race.py to call record_game_data

--- turtle_race.py
+++ turtle_race.py
@@ -35,7 +35,7 @@ while Race:
                 print(f"You have Won the bet on {winning} turtle! the {winning} is the winner")
             else:
                 print(f"You lose! {winning} turtle is winner")
-
+            record_game_data(bet, winning)
         distance = random.randint(0, 10)
         turtle.forward(distance)
 
